5d3171f9add017a5f72d65b8006553d8fb836912
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-09 16:51:50 -0600

    adds migration for autors and model test

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -1,3 +1,9 @@
 require 'rails_helper'
 
-describe 
+describe Book, type: :model do
+  describe 'Validations' do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:pages)}
+    it {should validate_presence_of(:year)}
+  end
+end
