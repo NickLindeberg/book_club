8cb7bd0d045a24697a1ad1e40046c6ddc43f24b1
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-10 11:56:24 -0600

    create user model and test

--- spec/features/user_visits_a_book_index_spec.rb
+++ spec/features/user_visits_a_book_index_spec.rb
@@ -5,7 +5,6 @@ describe 'user visits book index' do
 
     author_1 = Author.create(name: "Mary Sue")
     
-
     book_1 = author_1.books.create(title: "A book about Joe", pages: 430, year: 1947)
     book_2 = author_1.books.create(title: "Sharks", pages: 200, year: 1965)
 
