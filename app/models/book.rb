8b9d84456773c04893e748bada343ef69993f071
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-10 16:57:12 -0600

    add delete link to book show page. ATP

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ApplicationRecord
   validates_presence_of :year
 
   has_many :book_authors
-  has_many :authors, through: :book_authors
-  has_many :reviews
+  has_many :authors, through: :book_authors, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
