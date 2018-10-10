45090b575c95700c0b228f302c11e07757c5d114
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-10 14:10:16 -0600

    create class methods for reviews and tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,12 @@ class Review < ApplicationRecord
 
     belongs_to :user
     belongs_to :book
+
+    def self.total_reviews
+        Review.count
+    end
+
+    def self.avg_score
+        average(:score)
+    end
 end
