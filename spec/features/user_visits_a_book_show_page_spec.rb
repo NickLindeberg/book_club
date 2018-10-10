7ea57c00991a384793d17524a4730881c535d340
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-10 16:02:46 -0600

    add reviews to book show. ATP

--- spec/features/user_visits_a_book_show_page_spec.rb
+++ spec/features/user_visits_a_book_show_page_spec.rb
@@ -13,4 +13,22 @@ describe 'User visits book show' do
     expect(page).to have_content(book_1.year)
     expect(page).to_not have_content(book_2.title)
   end 
+
+  it 'show the reviews for a single book' do
+    author_1 = Author.create(name: "Mary Sue")
+    book_1 = author_1.books.create(title: "A book about Joe", pages: 430, year: 1947)
+    user_1 = User.create(name: "Joey Fatone")
+    review_1 = book_1.reviews.create(title: "Sharks are fun", description: "Who doesn't love a good shark...?", score: 5, user: user_1)
+    review_2 = book_1.reviews.create(title: "Sharks are awful", description: "Only evil people love sharks...?", score: 1, user: user_1)
+
+    visit book_path(book_1)
+
+    expect(page).to have_content(review_1.title)
+    expect(page).to have_content(review_1.description)
+    expect(page).to have_content(review_1.score)
+
+    expect(page).to have_content(review_2.title)
+    expect(page).to have_content(review_2.description)
+    expect(page).to have_content(review_2.score)
+  end 
 end
