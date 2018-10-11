77752c2d5dfd4f17733b07cac87a2479a7791795
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-11 17:07:37 -0600

    updates user show page to have review content

--- spec/features/user_visits_a_user_show_page_spec.rb
+++ spec/features/user_visits_a_user_show_page_spec.rb
@@ -4,9 +4,10 @@ describe 'user visits user show page' do
   it 'shows information for one user' do
     author_1 = Author.create(name: "Mary Sue")
     book_1 = author_1.books.create(title: "A book about Joe", pages: 430, year: 1947)
+    book_2 = author_1.books.create(title: "A book about Jones", pages: 430, year: 1947)
     user_1 = User.create(name: "Joey Fatone")
     review_1 = book_1.reviews.create(title: "Sharks are fun", description: "Who doesn't love a good shark...?", score: 5, user: user_1)
-    review_2 = book_1.reviews.create(title: "Fish are better than Sharks", description: "Fish are friends", score: 3, user: user_1)
+    review_2 = book_2.reviews.create(title: "Fish are better than Sharks", description: "Fish are friends", score: 3, user: user_1)
 
     visit book_path(book_1)
 
@@ -19,5 +20,5 @@ describe 'user visits user show page' do
     expect(page).to have_content(review_2.title)
     expect(page).to have_content(review_2.score)
     expect(page).to have_content(review_2.description)
-  endg
+  end
 end
